393acd151d419db98d742818b068041374dd16c0
Vache Apinyan <apinyan.v@gmail.com>
2023-06-19 20:17:51 +0400

    feat: add header name, change style and add link

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h1> This is a my first <b>'Hello React'</b> project </h1>
+
+       <a href="https://github.com/Vach001/first-react-todos" target='_blank'> This is the project repo link! </a>
       </header>
     </div>
   );
