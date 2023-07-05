f2208d8af48cb589fc25936d55684aaf6cff0d6d
Vache Apinyan <apinyan.v@gmail.com>
2023-07-05 17:55:07 +0400

    feat(TodoList): add TodoList

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -1,14 +1,97 @@
-import TodoInput from "../TodoForm/TodoForm";
-import styles from "./Todo.module.css";
+import { useState, useReducer } from "react";
+import "./App.css";
+import TodoForm from "../TodoForm/TodoForm";
+import TodoList from "./TodoList";
+import TodoFooter from "./TodoFooter";
 
-export default function Todo() {
+function reducer(state, action) {
+  if (action.type === "add") {
+    return [
+      ...state,
+      {
+        id: Math.random(),
+        text: action.payload.text,
+        isCompleted: false,
+      },
+    ];
+  } else if (action.type === "delete") {
+    return state.filter((t) => t.id !== action.payload.id);
+  } else if (action.type === "clear-completed") {
+    return state.filter((todo) => !todo.isCompleted);
+  } else if (action.type === "update") {
+    return state.map((todo) => {
+      if (todo.id === action.payload.updatedTodo.id) {
+        return action.payload.updatedTodo;
+      }
+      return todo;
+    });
+  }
+}
+
+function Todo() {
+  const [todos, dispatch] = useReducer(reducer, [
+    {
+      id: Math.random(),
+      text: "Learn JS",
+      isCompleted: false,
+    },
+    {
+      id: Math.random(),
+      text: "Learn CSS",
+      isCompleted: false,
+    },
+    {
+      id: Math.random(),
+      text: "Learn React",
+      isCompleted: false,
+    },
+  ]);
+
   return (
-    <div>
-    <h1 className={styles.head}>MY TODO</h1>
-      <div className={styles.container}>
-        <TodoInput />
-        
-      </div>
-      </div>
+    <div className="App">
+      <header>
+        <h1 className="todoAppTitle">todos</h1>
+      </header>
+
+      <TodoForm
+        onAdd={(text) => {
+          dispatch({
+            type: "add",
+            payload: {
+              text: text,
+            },
+          });
+        }}
+      />
+      <TodoList
+        todos={todos}
+        onDelete={(todo) => {
+          dispatch({
+            type: "delete",
+            payload: {
+              id: todo.id,
+            },
+          });
+        }}
+        onChange={(newTodo) => {
+          dispatch({
+            type: "update",
+            payload: {
+              updatedTodo: newTodo,
+            },
+          });
+        }}
+      />
+      <TodoFooter
+        todos={todos}
+        onClearCompleted={() => {
+          dispatch({
+            type: "clear-completed",
+          });
+        }}
+      />
+    </div>
   );
 }
+
+export default Todo;
