6f0534f67f2c24a4f896fcbd9b9499380475bda2
Vache Apinyan <apinyan.v@gmail.com>
2023-07-12 02:29:04 +0400

    feat(Todo): fix bug

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -4,30 +4,6 @@ import TodoForm from "../TodoForm/TodoForm";
 import TodoList from "../TodoList/TodoList";
 import TodoFooter from "../TodoFooter/TodoFooter";
 
-function reducer(state, action) {
-  if (action.type === "add") {
-    return [
-      ...state,
-      {
-        id: Math.random(),
-        text: action.payload.text,
-        isCompleted: false,
-      },
-    ];
-  } else if (action.type === "delete") {
-    return state.filter((t) => t.id !== action.payload.id);
-  } else if (action.type === "clear-completed") {
-    return state.filter((todo) => !todo.isCompleted);
-  } else if (action.type === "update") {
-    return state.map((todo) => {
-      if (todo.id === action.payload.updatedTodo.id) {
-        return action.payload.updatedTodo;
-      }
-      return todo;
-    });
-  }
-}
-
 function Todo() {
   const [todos, dispatch] = useReducer(
     todoReducer,
